6c94cb2c00c5d4b44635923a07bd66adcd3ad336
Enzo Moura <12929052716@cefet-rj.br>
2024-06-02 11:33:47 -0300

    Added find_arduino function and refactored directories.

--- PYBackEnd/arduino_communication/utils.py
+++ PYBackEnd/arduino_communication/utils.py
@@ -4,6 +4,6 @@ import serial.tools.list_ports
 def find_arduino_port():
     ports = list(serial.tools.list_ports.comports())
     for port in ports:
-        if 'Arduino' in port.description:
+        if 'Arduino' in port.description or 'Mega' in port.description or 'VID:PID' in port.hwid:
             return port.device
     return None

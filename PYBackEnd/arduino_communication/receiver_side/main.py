26f4ffef36955a59fb2b0e5b752b11803d7e0fe8
Enzo Moura <mouraenzo21@gmail.com>
2024-08-08 00:35:57 -0300

    Fixes on message reception.

--- PYBackEnd/arduino_communication/receiver_side/main.py
+++ PYBackEnd/arduino_communication/receiver_side/main.py
@@ -13,6 +13,53 @@ from beacon.model import Beacon as BeaconModel
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 BEACONLEN = 10
 
+
+def receive_and_store_message(arduino_port, use_my_sql=False):
+    global message_len
+    if arduino_port:
+        print(f"Arduino found on port: {arduino_port}")
+
+        with serial.Serial(arduino_port, 9600, timeout=5) as ser:
+            buffer = b''
+            while True:
+                if ser.in_waiting > 0:
+                    buffer += ser.read(ser.in_waiting)
+                    while len(buffer) >= message_len:
+                        header_index = buffer.find(b'\xFF\xFF')
+                        if header_index == -1:
+                            # Se não encontrar o cabeçalho, limpe o buffer para evitar dados antigos
+                            buffer = b''
+                            continue
+                        elif header_index > 0:
+                            # Se encontrar o cabeçalho mas não estiver no início, remova bytes anteriores
+                            buffer = buffer[header_index:]
+                        if len(buffer) >= message_len:
+                            response = buffer[:message_len]
+                            print("Received message from Arduino:", ' '.join(format(x, '02X') for x in response))
+
+                        if response[:2] == b'\xFF\xFF':
+                            print(response)
+                            message = response[2:(message_len - 2)]
+                            received_checksum = response[(message_len - 2):message_len]
+                            parsed_data = MessageModel.parse(message)
+                            print(f"Deserialized message:{parsed_data}\nCheck Sum: {received_checksum}")
+                            success = False if not MessageModel.vef_checksum(message, received_checksum) else True
+                            print(
+                                f"{'Error' if not success else 'Success'} handling message. Checksum {'differs.' if not success else 'is equal.'}")
+
+                            store_message(parsed_data, success, use_my_sql)
+                            return 1
+                        else:
+                            print("Incorrect Header, ignoring message:",
+                                  ' '.join(format(x, '02X') for x in response))
+                            store_message(None, False, use_my_sql)
+                            return 1
+
+            else:
+                return 0
+    else:
+        print("Arduino not found")
+
 def send_beacon(arduino_port):
     if arduino_port:
         print(f"Arduino found on port: {arduino_port}")
